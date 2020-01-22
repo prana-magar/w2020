ad0336c5db9a96e7e2124ca44f483287280619f7
MacStudent <macstudent@128-iMAC04.local>
2020-01-22 18:25:45 -0500

    Initial commit

---
+++ src/main/java/HWorld.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class HWorld {
+    public static void main(String[] args) {
+        System.out.println("aasdas" );
+
+
+        int a = 100;
+
+    }
+
+
+
+
+}
---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,17 @@
+package com.lambton;
+
+import java.util.Arrays;
+
+public class FirstExample {
+    public static void main(String[] args) {
+        int a = 100;
+        int b;
+        b = 200;
+        int c = a+b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println("Sum = "+ (a +b));
+
+    }
+}
