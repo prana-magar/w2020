0308a3f1aba15780cc198cc052842ec0ba096476
rana <prakashrana9847067614@gmail.com>
2020-01-22 18:49:40 -0500

    data types

--- src/main/java/com/lambton/FirstExample.java
+++ src/main/java/com/lambton/FirstExample.java
@@ -12,6 +12,26 @@ public class FirstExample {
         System.out.println(b);
         System.out.println(c);
         System.out.println("Sum = "+ (a +b));
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+
+        float f1 = 200.0F;
+        double d1 = 200.0;
+
+        int il = 10/3;
+        float fl = ((float)10/3);
+        float fll = (10f/3);
+        double dll = 10.0/3.0f;
+        int ar = 1231231231;
+        short rb = (short)ar;
+
+        double tt = 10_00_00.0;
+
+        System.out.println(ar);
+        System.out.println(rb);
+        System.out.println(tt);
+
 
     }
 }
